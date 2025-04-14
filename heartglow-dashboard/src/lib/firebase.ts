9bdc7c4b2bc216cb7eb72d51e194aece81ddbc11
Dale Kolnitys <dalekolnitys@gmail.com>
2025-04-14 18:24:55 -0400

    feat: Initialize Firebase Analytics

--- heartglow-dashboard/src/lib/firebase.ts
+++ heartglow-dashboard/src/lib/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBZiJPTs7dMccVgFV-YoTejnhy1bZNFEQY",
@@ -16,4 +17,6 @@ const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-export { app, auth, db }; 
+const analytics = isSupported().then(yes => yes ? getAnalytics(app) : null);
+
+export { app, auth, db, analytics }; 
